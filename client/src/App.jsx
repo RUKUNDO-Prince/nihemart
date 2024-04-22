39745db336770b39459b0bf3320536c4afac73fb
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-04-22 09:51:20 +0200

    finished routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home, About, Contact, Help, Products, Product, Cart, NotFound } from './pages';
 
 const App = () => {
   return (
     <div>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        
+        {/* Route for handling 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Help from "./Help";
+import Products from "./Products";
+import Product from "./Product";
+import Cart from "./Cart";
+import NotFound from "./NotFound";
+
+export { Home, About, Contact, Help, Products, Product, Cart, NotFound }
