98e3b213c7d531e4c2fece2fac29f1e1cecfb0f8
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-04-23 10:43:55 +0200

    making the navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { logo, cart, cartBg, likes } from "../assets";
+import { Link } from "react-router-dom";
+import Search from "./Search";
+import Languages from "./Languages";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="flex justify-center">
+      <img src={logo} alt="logo" />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/help">Help</Link>
+      </nav>
+      <Search />
+      <Languages />
+      <img src={likes} alt="likes" />
+      <img src={cartBg} alt="cart" />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
