20a96b75084a7729242cb636fc626ce6dfeba73b
ineza2 <ineza2ineza@gmail.com>
2024-04-24 21:36:30 +0200

    product related ops

---
+++ server/controllers/productController.js
@@ -0,0 +1,131 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const Product = require("../models/Product");
+const AdminPanel=require("../models/adminPanel")
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const addProduct = async (req, res) => {
+  try {
+    if (!req.user.isAdmin) {
+      return res.status(403).json({ message: "Only admin can add products" });
+    }
+
+    const { name, description, price, quantity } = req.body;
+    const photos = req.files.map((file) => file.path);
+
+    const product = new Product({
+      name,
+      description,
+      price,
+      quantity,
+      photos,
+    });
+
+    await product.save();
+
+    res.status(201).json({ message: "Product added successfully", product });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const likeProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const userId = req.user._id;
+
+    const { name, email, phoneNumber, location } = req.body;
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    if (product.likes.includes(userId)) {
+      return res
+        .status(400)
+        .json({ message: "You have already liked this product" });
+    }
+
+    product.likes.push({
+      user: userId,
+      userInfo: { name, email, phoneNumber, location },
+    });
+    await product.save();
+
+    const userInfo = { name, email, phoneNumber, location };
+    adminPanelDisplay(userInfo);
+
+    res.status(200).json({ message: "Product liked successfully", product });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const adminPanelDisplay = async (userInfo) => {
+  try {
+    let adminPanel = await AdminPanel.findOne({ email: userInfo.email });
+    if (!adminPanel) {
+      adminPanel = new AdminPanel(userInfo);
+    } else {
+      adminPanel.name = userInfo.name;
+      adminPanel.phoneNumber = userInfo.phoneNumber;
+      adminPanel.location = userInfo.location;
+    }
+    await adminPanel.save();
+  } catch (error) {
+    console.error("Error displaying user information to admin panel:", error);
+  }
+};
+
+
+
+
+// const likeProduct = async (req, res) => {
+//   try {
+//     const { productId } = req.params;
+//     const userId = req.user._id;
+
+//     const product = await Product.findById(productId);
+
+//     if (!product) {
+//       return res.status(404).json({ message: "Product not found" });
+//     }
+
+//     if (product.likes.includes(userId)) {
+//       return res
+//         .status(400)
+//         .json({ message: "You have already liked this product" });
+//     }
+
+//     product.likes.push(userId);
+//     await product.save();
+
+//     res.status(200).json({ message: "Product liked successfully", product });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// };
+
+
+
+module.exports = { addProduct, likeProduct, adminPanelDisplay };
+module.exports = upload;
---
+++ server/models/product.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  phoneNumber: String,
+  location: String,
+});
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  photos: [
+    {
+      type: String,
+    },
+    ],
+    likes: [{
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    userInfo: {
+      type: userSchema,
+    }
+  }],
+  ratings: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+      },
+    },
+  ],
+});
+
+
+productSchema.virtual("averageRating").get(function () {
+  if (this.ratings.length === 0) return 0;
+
+  const sum = this.ratings.reduce((total, rating) => total + rating.rating, 0);
+  return sum / this.ratings.length;
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
