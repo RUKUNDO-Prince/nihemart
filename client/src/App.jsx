3957dd9048ce4b61a92f1b783bb988f62fa6e056
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-04-25 15:56:36 +0200

    initialised the project and added some packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,7 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Home, About, Contact, Help, Products, Product, Cart, NotFound } from './pages';
-import { Footer, Navbar } from '../components';
-
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        
-        {/* Route for handling 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
-};
-
-export default App;
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
