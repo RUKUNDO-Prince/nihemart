ed176b694e7a8fea1015546f5515bf58279fee37
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-04-25 16:43:51 +0200

    finished the layout of the navbar, heading to some default styles

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { logo, cart, cartBg, likes } from '../assets'
+import { Link } from 'react-router-dom'
+import Search from './Search'
+import Languages from './Languages'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='flex justify-between gap-3 items-center m-5 shadow-xl rounded-lg'>
+      <img src={logo} alt="logo" />
+      <ul className='flex gap-4'>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
+        <Link to={"/help"}>Help</Link>
+      </ul>
+      <Search />
+      <Languages />
+      <div className='flex'>
+        <img src={likes} alt="likes" />
+        <img src={cartBg} alt="cart" />
+      </div>
     </div>
   )
 }
