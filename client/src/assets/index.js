63f44e801d800370a66bcbee1f43540a250438da
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-04-25 16:16:18 +0200

    added some assets

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg'
+import cartBg from './cart-bg.svg'
+import cart from './cart.svg'
+import likes from './likes.svg'
+
+export {
+    logo,
+    cartBg,
+    cart,
+    likes
+}
