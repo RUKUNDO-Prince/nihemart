13172af473c964237f1850fbb0da16a404698377
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-04-27 20:58:33 +0300

    added a mockup for the dropdown

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -8,6 +8,16 @@ const Dropdown = ({ data }) => {
   return (
     <div className='relative flex flex-col items-center w-[340px] h-[340px] rounded-lg'>
         <button onClick={() => setIsOpen((prev) => !prev)} className='bg-blue p-4 w-full flex items-center justify-between font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white'>Dropdown {!isOpen ? (<AiOutlineCaretDown className="h-8" />) : (<AiOutlineCaretUp className="h-8" />)}</button>
+        {isOpen && (
+          <div className='bg-blue absolute top-20 flex flex-col items-start rounded-lg p-2 w-full'>
+            {data.map((item, index) => (
+              <div className='flex w-full justify-between p-4 hover:bg-primary hover:border-l-white border-l-4 cursor-pointer rounded-r-lg border-l-transparent' key={index}>
+                <h3 className='font-bold'>{item.name}</h3>
+                <h3>{item.icon}</h3>
+              </div>
+            ))}
+          </div>
+        )}
     </div>
   )
 }
