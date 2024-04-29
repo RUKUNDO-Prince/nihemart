6c40305189035e0bd6e7b9fd0b2c16bf223e2db8
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-04-29 12:10:10 +0300

    finished the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <footer className="bg-gradient-to-b from-blue to-dark">
       <Newsletter />
-      <hr />
+      <div className="w-full h-[1.5px] bg-white"></div>
       {/* MAIN FOOTER */}
       <div>
         <div className=" h-[30vh] content-center">
@@ -19,13 +19,13 @@ const Footer = () => {
           <div className="w-[20%] flex">
           <img src={logo} alt="logo" />
           <div>
-            <h1 className="text-white font-bold">Nihe Mart</h1>
+            <h1 className="text-white font-bold text-[1.2em]">Nihe Mart</h1>
             <p className="text-white">Buy Goods and order on our commercial website</p>
           </div>
           </div>
           {footerLinks.map((item, index) => (
             <div key={index} className="flex flex-col">
-              <h1 className="text-white font-bold">{item.title}</h1>
+              <h1 className="text-white font-bold text-[1.2em]">{item.title}</h1>
               {item.links.map((link, index) => (
                 <Link target="_blank" className="text-white hover:opacity-[80%] hover:underline transition-opacity" key={index} to={link.link}>{link.text}</Link>
               ))}
@@ -33,14 +33,14 @@ const Footer = () => {
           ))}
           {footerActions.map((item, index) => (
             <div className="flex flex-col" key={index}>
-              <h1 className="text-white font-bold">{item.title}</h1>
+              <h1 className="text-white font-bold text-[1.2em]">{item.title}</h1>
               {item.links.map((link, index) => (
                 <Link target="_blank" className="text-white hover:opacity-[80%] hover:underline transition-opacity" key={index} to={link.link}>{link.text}</Link>
               ))}
             </div>
           ))}
           <div>
-            <h1 className="text-white font-bold mb-3">Find Us Online</h1>
+            <h1 className="text-white font-bold mb-3 text-[1.2em]">Find Us Online</h1>
             <div className="flex gap-5">
               <Link target="_blank" to={"https://instagram.com"}><FaInstagram color="white" size={30} /></Link>
               <FaWhatsapp color="white" size={30} />
@@ -49,11 +49,11 @@ const Footer = () => {
           </div>
         </div>
           </div>
-        <hr />
+        <div className="w-[70%] m-auto h-[0.7px] bg-gray-70"></div>
         {/* FOOTER END */}
-        <div className="flex justify-around items-center h-[15vh]">
+        <div className="flex justify-between items-center h-[15vh] w-[70%] m-auto">
           <p className="text-white">
-            Copyright © 2025 Nihe Mart . All rights reserved
+            Copyright © 2024 Nihe Mart . All rights reserved
           </p>
           <p className="flex gap-5">
             {footerEndData.map((item, index) => (
--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { push } from '../assets'
 
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
+    <div className='flex flex-col justify-center items-center my-5'>
+      <h1 className='text-white my-5 font-libre text-[3em]'>Subscribe to our newsletter</h1>
+      <p className='text-white '>Keep updated on new Products and whatever happening on Nihe Mart </p>
+      <div className='w-[40%] flex justify-between p-[5px] items-center my-5 border-none rounded-md bg-glass2'>
+        <input type="email" placeholder='Email address' className='bg-transparent w-full outline-none text-white mx-2' />
+        <button className='bg-blue2 p-[10px] rounded-md'><img src={push} alt="icon" /></button>
+      </div>
+    </div>
   )
 }
 
