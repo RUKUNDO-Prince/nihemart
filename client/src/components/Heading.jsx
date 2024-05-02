8b4507d0c2a91e8b266fcd6a4a6bdc924d6aa377
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-05-02 17:13:45 +0300

    added a heading and subheading component

---
+++ client/src/components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Heading = ({ title }) => {
+  return (
+    <h1 className='font-inter font-bold text-black text-[2.5em]'>{title}</h1>
+  )
+}
+
+export default Heading
