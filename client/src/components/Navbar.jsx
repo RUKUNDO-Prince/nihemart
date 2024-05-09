f14b02a359ac2d9aea7d6a986da02434bd5c2e46
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-05-09 15:40:43 +0300

    some minor changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { logo, cart, cartBg, likes } from '../assets';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Search from './Search';
 import Languages from './Languages';
 
 const Navbar = () => {
   return (
     <div className='flex justify-between gap-3 items-center m-5 shadow-xl rounded-lg px-10'>
-      <img src={logo} alt="logo" />
+      <Link to="/"><img src={logo} alt="logo" /></Link>
       <ul className='flex gap-10 font-poppins'>
         <NavLink exact to={"/"} activeclassname="active" className='hover:text-primary transition-colors'>Home</NavLink>
         <NavLink to={"/about"} activeclassname="active" className='hover:text-primary transition-colors'>About</NavLink>
@@ -16,9 +16,9 @@ const Navbar = () => {
       </ul>
       <Search />
       <Languages />
-      <div className='flex gap-5'>
-        <img src={likes} alt="likes" />
-        <img src={cartBg} alt="cart" />
+      <div className='flex gap-5 items-center'>
+        <Link to="/likes"><img src={likes} alt="likes" /></Link>
+        <Link to="/cart"><img src={cartBg} alt="cart" /></Link>
       </div>
     </div>
   );
