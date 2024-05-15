69ac674faa63f22f5c9a923e11f5597c91908bc8
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-05-15 09:24:46 +0300

    finished routing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { AddProduct, Analytics, Login, NotFound, Notifications, Orders, Product, Products, Profile, Signup } from './pages'
+import { Footer, Navbar } from './components'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/signup"} element={<Signup />} />
+      </Routes>
+        <>
+          <Navbar />
+          <Routes>
+            <Route path={"/"} element={<Analytics />} />
+            <Route path={"/addProduct"} element={<AddProduct />} />
+            <Route path={"/notifications"} element={<Notifications />} />
+            <Route path={"/orders"} element={<Orders />} />
+            <Route path={"/products"} element={<Products />} />
+            <Route path={"/profile"} element={<Profile />} />
+            <Route path={"/product/:id"} element={<Product />} />
+            <Route path={"*"} element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </>
+    </>
   )
 }
 
