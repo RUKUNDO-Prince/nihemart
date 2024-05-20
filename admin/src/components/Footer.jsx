dc624e97a78f555e88d9a80ab2e7ba16b89096ab
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-05-20 14:26:57 +0300

    finished the footer

--- admin/src/components/Footer.jsx
+++ admin/src/components/Footer.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='flex justify-between px-8 py-4 bg-[#002B53] text-white absolute bottom-0 right-0 left-0'>
+      <div>
+        <ul className='flex'>
+          <li className='footer-list-item'>Terms of service</li>
+          <li className='footer-list-item'>User agreement</li>
+          <li className='footer-list-item'>Privacy policy</li>
+        </ul>
+      </div>
+      <div>
+        <p>Copyright © Nihe Mart 2023. All rights reserved</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
