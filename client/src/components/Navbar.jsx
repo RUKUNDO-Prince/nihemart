a1146081011650ab2e4b2329ec05ce950a247733
LuckyBelieve <luckybelieve10@gmail.com>
2024-05-21 21:28:26 +0200

    complete sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,11 +4,20 @@ import { Link, NavLink } from "react-router-dom";
 import Search from "./Search";
 import Languages from "./Languages";
 import { Icon } from "@iconify/react";
+import { FaX } from "react-icons/fa6";
 
 const Navbar = () => {
   const [search, setSearch] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const handleSidebarOpen = () => {
+    setSidebarOpen(true);
+  };
+  const handleClose = () => {
+    setSidebarOpen(false);
+  };
   return (
-    <div className="flex justify-between gap-3 items-center m-5 shadow-lg rounded-lg px-5 relative">
+    <div className="flex justify-between gap-3 items-center md:m-5 shadow-lg rounded-lg px-5">
       <Link to="/" className="w-[82px] h-[87px]">
         <img
           src={logo}
@@ -59,7 +68,9 @@ const Navbar = () => {
         >
           <Icon icon={"material-symbols-light:search"} className="w-6 h-6" />
         </div>
+        <div className="hidden md:block">
         <Languages />
+        </div>
         <Link
           to={"/likes"}
           className="w-10 h-10 flex items-center justify-center"
@@ -81,7 +92,7 @@ const Navbar = () => {
         </Link>
         <div
           className="flex items-center justify-center md:hidden p-2 rounded-full hover:bg-gray-100"
-          onClick={() => setSearch(false)}
+          onClick={() => handleSidebarOpen()}
         >
           <Icon icon={"clarity:bars-line"} fontSize={24} />
         </div>
@@ -101,9 +112,66 @@ const Navbar = () => {
         >
           <Icon icon={"iconoir:cancel"} fontSize={24} />
         </div>
-        
+      </div>
+      {/* mobile nav */}
+      <div
+        id="sidebar"
+        onClick={handleClose}
+        className={` ${
+          sidebarOpen ? "block" : "hidden"
+        } fixed md:hidden inset-0 h-screen bg-black bg-opacity-50 z-30 backdrop-blur-sm duration-200`}
+      ></div>
+      <div
+        className={`${
+          sidebarOpen ? "right-0 " : "-right-[100%]"
+        } z-30  fixed min-w-[320px] top-0 rounded-l-lg bottom-0 bg-white p-5  transition-all duration-500 md:hidden`}
+      >
+        <div className={"flex items-center justify-between"}>
+          <Link to={"/"}>
+            <img src="/logo.svg" alt="logo" className="w-[50px]"/>
+          </Link>
+          <Languages />
+          <div
+            onClick={() => setSidebarOpen(false)}
+            className="flex items-center justify-center border border-primary rounded-full p-2 hover:bg-primary hover:text-white"
+          >
+            <FaX />
+          </div>
+        </div>
+        <div className=" md:hidden flex flex-col gap-5 xl:gap-10 mt-5">
+        <NavLink
+          exact
+          to={"/"}
+          activeclassname="active"
+          className="hover:text-primary transition-colors"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to={"/about"}
+          activeclassname="active"
+          className="hover:text-primary transition-colors"
+        >
+          About
+        </NavLink>
+        <NavLink
+          to={"/contact"}
+          activeclassname="active"
+          className="hover:text-primary transition-colors"
+        >
+          Contact
+        </NavLink>
+        <NavLink
+          to={"/help"}
+          activeclassname="active"
+          className="hover:text-primary transition-colors"
+        >
+          Help
+        </NavLink>
+      </div>
       </div>
     </div>
+    
   );
 };
 
