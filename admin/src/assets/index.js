6bf4a97cdbd8b1aee0606f66a99ca7b1c6068ab3
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-05-21 19:41:05 +0300

    finished the analytics page

--- admin/src/assets/index.js
+++ admin/src/assets/index.js
@@ -1,9 +1,11 @@
 import figure from './figure.svg'
 import logo from './logo.svg'
 import profile from './profile.svg'
+import icon from './icon.svg'
 
 export {
     figure,
     logo,
-    profile
+    profile,
+    icon
 }
--- admin/src/pages/Analytics.jsx
+++ admin/src/pages/Analytics.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { icon } from '../assets'
+import { AiOutlineSwapRight } from "react-icons/ai";
 
 const Analytics = () => {
   return (
-    <div>Analytics</div>
+    <div>
+    <div className='grid grid-rows-1 grid-cols-4 gap-5 w-full p-8'>
+      <div className='bg-white border-[1px] border-gray-90 row-span-1 col-span-1 p-8 rounded-lg '>
+        <div className='flex items-center gap-3'>
+          <img src={icon} alt="icon" />
+          <p className='font-lato font-semibold text-16 text-[#5D6E8B]'>Total products</p>
+        </div>
+        <p className='font-lato font-semibold text-[48px]'>23</p>
+      </div>
+      <div className='bg-white border-[1px] border-gray-90 row-span-1 col-span-1 p-8 rounded-lg '>
+        <div className='flex items-center gap-3'>
+          <img src={icon} alt="icon" />
+          <p className='font-lato font-semibold text-16 text-[#5D6E8B]'>Products updated</p>
+        </div>
+        <p className='font-lato font-semibold text-[48px]'>15</p>
+      </div>
+      <div className='bg-white border-[1px] border-gray-90 row-span-1 col-span-1 p-8 rounded-lg '>
+        <div className='flex items-center gap-3'>
+          <img src={icon} alt="icon" />
+          <p className='font-lato font-semibold text-16 text-[#5D6E8B]'>Orders</p>
+        </div>
+        <p className='font-lato font-semibold text-[48px] '>96</p>
+      </div>
+      <div className='bg-white border-[1px] border-gray-90 row-span-1 col-span-1 p-8 rounded-lg '>
+        <div className='flex items-center gap-3'>
+          <img src={icon} alt="icon" />
+          <p className='font-lato font-semibold text-16 text-[#5D6E8B]'>Categories</p>
+        </div>
+        <p className='font-lato font-semibold text-[48px]'>9</p>
+      </div>
+    </div>
+    <button className='flex items-center bg-blue3 py-3 px-8 rounded-lg outline-none text-white m-8 float-end hover:bg-blue2'>Log out<AiOutlineSwapRight /></button>
+    </div>
   )
 }
 
