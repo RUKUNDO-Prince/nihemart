39b093f8e9b12d40b0dbe9e4eb4817313ec46320
LuckyBelieve <luckybelieve10@gmail.com>
2024-05-22 07:58:43 +0200

    complete navbar component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -5,7 +5,7 @@ import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 import { PiCaretDownBold, PiCaretUpBold } from "react-icons/pi";
 import { Icon } from "@iconify/react";
 
-const Search = ({ search=true }) => {
+const Search = ({ search = true }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -26,10 +26,19 @@ const Search = ({ search=true }) => {
           className="flex items-center justify-between w-full px-2 duration-300 active:text-gray-60 gap-1"
           onClick={() => setIsOpen((prev) => !prev)}
         >
-          categories {isOpen ? <PiCaretUpBold /> : <PiCaretDownBold />}
+          categories{" "}
+          <PiCaretUpBold
+            className={`${
+              isOpen ? "rotate-180" : "rotate-0"
+            } transition-all duration-300`}
+          />
         </button>
-        {isOpen && search && (
-          <div className="absolute top-20 bg-glass flex right-0 flex-col items-start rounded-lg p-2 w-[100%] z-30">
+        {search && (
+          <div
+            className={`absolute top-20 bg-glass flex right-0 flex-col items-start rounded-lg p-2 w-[100%] z-30 ${
+              isOpen ? "scale-100" : "scale-0"
+            } origin-top transition-all duration-300 z-40`}
+          >
             {categories.map((category, index) => (
               <div
                 className="flex w-full justify-between p-2 border border-transparent hover:bg-glass3 cursor-pointer rounded-r-lg border-l-transparent"
