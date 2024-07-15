d6409899e0b9b07b9bdc7c92ae5fc4f3d6285b42
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-07-15 17:07:22 +0300

    some minor changes

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -5,12 +5,12 @@ const NotFound = () => {
   return (
     <div className='m-[50px]'>
       <p className="text-gray-90 mb-[20px]">
-        Home / <span className="text-black">NotFound</span>
+        Ahabanza / <span className="text-black">Ntiyabonetse</span>
       </p>
       <div className='flex flex-col justify-center items-center'>
-        <h1 className='font-inter font-medium text-[110px]'>404 Not Found</h1>
-        <p className='font-poppins font-regular text-[16px]'>Your requested page not found. You may go home page.</p>
-        <Link to="/" className='bg-primary text-white mt-9 py-3 px-8 rounded-md hover:bg-opacity-[80%]'>Back to Homepage</Link>
+        <h1 className='font-inter font-medium text-[110px]'>404 Paje Ntayihari</h1>
+        <p className='font-poppins font-regular text-[16px]'>Paje muri gushaka ntago yabashije kuboneka. Musubire ahabanza.</p>
+        <Link to="/" className='bg-primary text-white mt-9 py-3 px-8 rounded-md hover:bg-opacity-[80%]'>Ahabanza</Link>
       </div>
     </div>
   )
