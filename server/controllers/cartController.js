d8b10f8a83dac986f096c061d5bd5adb41eb4952
LuckyBelieve <luckybelieve10@gmail.com>
2024-07-25 13:43:28 +0200

    cartProgress

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -6,7 +6,6 @@ const addToCart = async (req, res) => {
     const { productId } = req.params;
     const { _id: userId } = req.user;
 
-    console.log(userId);
     if (!userId) {
       return res.status(400).json({ message: "User ID is required" });
     }
@@ -62,7 +61,10 @@ const getCart = async (req, res) => {
       return res.status(404).json({ message: "Cart not found" });
     }
 
-    return res.status(200).json({ cart });
+
+    return res.status(200).json({
+      cart
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
