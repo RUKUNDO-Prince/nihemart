8e58cf7b55a354b70cc703c4c21204987aab84d5
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-07-29 16:06:28 +0300

    changes in the cart page

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -35,65 +35,67 @@ const Cart = () => {
           Loading...
         </div>
       ) : cartItems?.length !== 0 ? (
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 min-w-[1012px]">
-          <thead className="text-[16px] text-black font-poppins drop-shadow-lg my-[20px]">
-            <tr className="shadow-md">
-              <th scope="col" className="px-6 py-8">
-                Product
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Quantity
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Subtotal
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {cart.length !== 0 ? (
-              cart?.map((item, index) => (
-                <tr key={index} className="bg-white border-b">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white flex gap-5 items-center"
-                  >
-                    <img
-                      src={`${api + "/" + item?.photo}`}
-                      className="w-[60px]"
-                      alt="img"
-                    />{" "}
-                    <p className="text-[16px] text-black">{item.name}</p>
-                  </th>
-                  <td className="px-6 py-4">
-                    {displayNumbers(item.price)} Frw
-                  </td>
-                  <td className="px-6 py-4">
-                    {displayNumbers(item.quantity)} items
-                  </td>
-                  <td className="px-6 py-4">
-                    {displayNumbers(item.subtotal)} Frw
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>Ntakintu kiri mu gatebo</tr>
-            )}
-          </tbody>
-        </table>
+        <>
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 min-w-[1012px]">
+            <thead className="text-[16px] text-black font-poppins drop-shadow-lg my-[20px]">
+              <tr className="shadow-md">
+                <th scope="col" className="px-6 py-8">
+                  Product
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Quantity
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Subtotal
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.length !== 0 ? (
+                cart?.map((item, index) => (
+                  <tr key={index} className="bg-white border-b">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white flex gap-5 items-center"
+                    >
+                      <img
+                        src={`${api + "/" + item?.photo}`}
+                        className="w-[60px]"
+                        alt="img"
+                      />{" "}
+                      <p className="text-[16px] text-black">{item.name}</p>
+                    </th>
+                    <td className="px-6 py-4">
+                      {displayNumbers(item.price)} Frw
+                    </td>
+                    <td className="px-6 py-4">
+                      {displayNumbers(item.quantity)} items
+                    </td>
+                    <td className="px-6 py-4">
+                      {displayNumbers(item.subtotal)} Frw
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>Ntakintu kiri mu gatebo</tr>
+              )}
+            </tbody>
+          </table>
+          <Link
+            to="/tumiza"
+            className="bg-blue3 px-5 py-2 md:px-[30px] md:py-[10px] rounded-md text-white hover:bg-blue2 my-5 float-end"
+          >
+            Tumiza Ibi Bicuruzwa
+          </Link>
+        </>
       ) : (
-        <div className="h-[500px] flex items-center justify-center">
-          Ntakintu kiri mu gitebo
+        <div className="h-[215px] flex items-center justify-center">
+          Ntakintu kiri mu gatebo
         </div>
       )}
-      <Link
-        to="/tumiza"
-        className="bg-blue3 px-5 py-2 md:px-[30px] md:py-[10px] rounded-md text-white hover:bg-blue2 my-5 float-end"
-      >
-        Tumiza Ibi Bicuruzwa
-      </Link>
     </div>
   );
 };
