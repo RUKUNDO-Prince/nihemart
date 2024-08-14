3a702c39370a741f7205c8a2d9d1731b825989fa
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-08-14 12:43:40 +0300

    finished translations for the homepage

--- client/src/components/Arrivals.jsx
+++ client/src/components/Arrivals.jsx
@@ -3,14 +3,17 @@ import SubHeading from "./SubHeading";
 import Heading from "./Heading";
 import ArrivalsGrid from "./ArrivalsGrid";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const Arrivals = () => {
+  const { t } = useTranslation();
+
   return (
     <div className=" p-5 md:p-[50px]">
-      <SubHeading title="Tangira ugure" />
+      <SubHeading title={t('buying')} />
       <div className="flex justify-between items-center">
         <Heading title="Top categories" />
-        <Link to="/categories" className="bg-blue3 px-5 py-2 md:px-[30px] md:py-[10px] rounded-md text-white hover:bg-blue2">Reba category zose</Link>
+        <Link to="/categories" className="bg-blue3 px-5 py-2 md:px-[30px] md:py-[10px] rounded-md text-white hover:bg-blue2">{t('categories')}</Link>
       </div>
       <ArrivalsGrid />
     </div>
--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -3,14 +3,17 @@ import SubHeading from "./SubHeading";
 import Heading from "./Heading";
 import { Link } from "react-router-dom";
 import ProductsList from "./ProductsList";
+import { useTranslation } from "react-i18next";
 
 const Products = () => {
+  const { t } = useTranslation();
+
   return (
     <div className=" p-5 md:p-[50px]">
-      <SubHeading title="Ibicuruzwa byacu" />
+      <SubHeading title={t('products')} />
       <div className="flex justify-between items-center">
-        <Heading title="Ibyasohotse uku kwezi" />
-        <Link to="/ibicuruzwa-byose" className="bg-blue3 px-5 py-2 md:px-[30px] md:py-[10px] rounded-md text-white hover:bg-blue2">Reba byose</Link>
+        <Heading title={t('month')} />
+        <Link to="/ibicuruzwa-byose" className="bg-blue3 px-5 py-2 md:px-[30px] md:py-[10px] rounded-md text-white hover:bg-blue2">{t('view')}</Link>
       </div>
       <ProductsList maxProducts={12}/>
     </div>
